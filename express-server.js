52ec6991dbd7d79c6f4abdb3f3f8c916953f01f8
Ryan Vargas <i.am.ryan.v@gmail.com>
2020-06-03 08:22:15 -0700

    Updating remote repository

--- express-server.js
+++ express-server.js
@@ -1,12 +1,12 @@
-"use strict";
+'use strict';
 
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express');
+const morgan = require('morgan');
 const app = express();
-const store = require("./playstore");
-app.use(morgan("common"));
+const store = require('./playstore');
+app.use(morgan('common'));
 
-app.get("/apps", (req, res) => {
+app.get('/apps', (req, res) => {
   let responseData = store;
   const { sort, genres } = req.query;
 
@@ -30,5 +30,5 @@ app.get("/apps", (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log("Express server is listening on port 3000!");
+  console.log('Express server is listening on port 3000!');
 });
